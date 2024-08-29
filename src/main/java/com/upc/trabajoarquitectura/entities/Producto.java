240d552e0205706a252a094e356f638ba12e7b00
ncad24 <149948740+ncad24@users.noreply.github.com>
2024-08-28 20:37:34 -0500

    Inicializando productos

--- src/main/java/com/upc/trabajoarquitectura/entities/Producto.java
+++ src/main/java/com/upc/trabajoarquitectura/entities/Producto.java
@@ -1,5 +1,8 @@
 package com.upc.trabajoarquitectura.entities;
 
 public class Producto {
-
+    public String nombre;
+    public String descripcion;
+    public int precio;
+    public Producto(String nombre, String descripcion, int precio) {}
 }
