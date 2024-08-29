9a3ebf7cb7273756dcbc93661ff7985b3c27b4fb
ncad24 <149948740+ncad24@users.noreply.github.com>
2024-08-29 09:11:35 -0500

    Agregando nuevas entidades

---
+++ src/main/java/com/upc/trabajoarquitectura/entities/Categoria.java
@@ -0,0 +1,17 @@
+package com.upc.trabajoarquitectura.entities;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Table(name="categorias")
+public class Categoria {
+}
---
+++ src/main/java/com/upc/trabajoarquitectura/entities/Supermercado.java
@@ -0,0 +1,4 @@
+package com.upc.trabajoarquitectura.entities;
+
+public class Supermercado {
+}
