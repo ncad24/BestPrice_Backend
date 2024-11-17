3d9e5bb76da5fc40144137941ab5ca576bbf86e5
GabrielX_64 <gabrielcajaslara2004@gmail.com>
2024-11-17 17:20:14 -0500

    Eliminando la entidad, el repositorio, servicio, dto y controlador de descuento

--- src/main/java/com/upc/trabajoarquitectura/respository/DescuentoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.upc.trabajoarquitectura.respository;
-
-import com.upc.trabajoarquitectura.entities.Descuento;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DescuentoRepository extends JpaRepository<Descuento, Long> {
-}
